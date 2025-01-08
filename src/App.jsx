66b1c79dfe32e675506f845ac462b8c9d8a3a396
Whoplaros <whoplaros@gmail.com>
2025-01-08 08:19:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./assets/ayww-logo.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo img-fluid" alt="logo" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
