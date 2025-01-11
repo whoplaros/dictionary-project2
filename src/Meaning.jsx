8fc2a96abad31ec11c0cb409b9e93b7f81c7fa37
Whoplaros <whoplaros@gmail.com>
2025-01-11 08:54:22 +0200

    Rendered the word meanings

---
+++ src/Meaning.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Meaning(props) {
+	console.log(props.meaning);
+	return (
+		<div className="Meaning">
+			<h3>{props.meaning.partOfSpeech}</h3>
+			<p>
+				{props.meaning.definition}
+				<br />
+				<em>{props.meaning.example}</em>
+			</p>
+		</div>
+	);
+}
--- src/Results.jsx
+++ src/Results.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning.jsx";
 
 export default function Result(props) {
 	if (props.results) {
@@ -6,7 +7,11 @@ export default function Result(props) {
 			<div className="Results">
 				<h2>{props.results.word}</h2>
 				{props.results.meanings.map(function (meaning, index) {
-					return meaning.definitions.definition;
+					return (
+						<div key={index}>
+							<Meaning meaning={meaning} />
+						</div>
+					);
 				})}
 			</div>
 		);
