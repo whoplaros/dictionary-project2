ee4e1b97fcde0de50bbe95110cd2e17ef0215590
Whoplaros <whoplaros@gmail.com>
2025-01-11 08:27:53 +0200

    typo in Results component

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 export default function Result(props) {
-   if(props.results) {
-    return (
-        <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-            return meaning.definitions[0].definition;
-        }0}
-        </div>
-    );
-} else {
-    return (null);
-}
+	if (props.results) {
+		return (
+			<div className="Results">
+				<h2>{props.results.word}</h2>
+				{props.results.meanings.map(function (meaning, index) {
+					return meaning.definitions.definition;
+				})}
+			</div>
+		);
+	} else {
+		return null;
+	}
 }
