f111857d6ca44ac37a229bc53a42dd962d05f0ac
Whoplaros <whoplaros@gmail.com>
2025-01-11 09:31:55 +0200

    Rendered Synonym component

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms.jsx";
 
 export default function Meaning(props) {
 	console.log(props.meaning);
 	return (
 		<div className="Meaning">
 			<h3>{props.meaning.partOfSpeech}</h3>
-			<p>
+			<div>
 				{props.meaning.definition}
 				<br />
+				<strong>Example: </strong>
 				<em>{props.meaning.example}</em>
-			</p>
+				<br />
+				<strong>Synonyms: </strong>
+				<Synonyms synonyms={props.meaning.synonyms} />
+			</div>
 		</div>
 	);
 }
